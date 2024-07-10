9222b185a0539134a328299be6c3ba5286724f85
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-10 01:44:45 -0300

    Update 17307825508.lua

--- 17307825508.lua
+++ 17307825508.lua
@@ -21,6 +21,21 @@ local function loadAndExecuteScript()
     end
 end
 
+local function removeHTMLTags(str)
+    return str:gsub("<[^>]->", "")
+end
+
+local function removeNonAlpha(str)
+    return str:gsub("[^%a%s]", "")
+end
+
+local function findWordPattern(text, pattern)
+    local cleanText = removeHTMLTags(text):lower()
+    cleanText = removeNonAlpha(cleanText)
+    local found = string.find(cleanText, pattern)
+    return found ~= nil
+end
+
 teleportToTargetPlace()
 
 spawn(function()
@@ -28,20 +43,37 @@ spawn(function()
 end)
 
 ReplicatedStorage.Fate.OnClientEvent:Connect(function(arg1, jobId)
-    if type(arg1) == "string" and string.find(arg1, "Sent<font size=\"0\">xd</font><font size=\"0\">xd</font>en<font size=\"0\">xd</font>ced to R<font size=\"0\">xd</font>IA.") then
-        local player = game.Players.LocalPlayer
-        player:Kick("No UGC - Rejoin")
-        
-        spawn(function()
-            if #Players:GetPlayers() <= 1 then
-                
-                wait()
-                TeleportService:Teleport(targetPlaceId, player)
-            else
-                TeleportService:TeleportToPlaceInstance(targetPlaceId, jobId, player)
+    if type(arg1) == "string" then
+        local patterns = {
+            "ria",
+            "r⁠⁠⁠ia",
+            "r⁠IA",
+            "r[⁠⁠⁠IA]",
+             "ri"
+        }
+
+        local foundMatch = false
+        for _, pattern in ipairs(patterns) do
+            if findWordPattern(arg1, pattern) then
+                foundMatch = true
+                break
             end
-        end)
-    else
-        print("Received unknown argument or incorrect type.")
+        end
+
+        if foundMatch then
+            local player = game.Players.LocalPlayer
+            player:Kick("No UGC - Rejoin")
+            
+            spawn(function()
+                if #Players:GetPlayers() <= 1 then
+                    wait()
+                    TeleportService:Teleport(targetPlaceId, player)
+                else
+                    TeleportService:TeleportToPlaceInstance(targetPlaceId, player)
+                end
+            end)
+        else
+            print("Received unknown argument or incorrect type.")
+        end
     end
 end)
