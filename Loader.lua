a5cbab31d63ee66aee509f6d2bc03f8dcd6ea4a8
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-10 12:40:21 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,25 +1,2 @@
-return function(id)
-    local function notify(message)
-        pcall(function()
-            game:GetService("StarterGui"):SetCore("SendNotification", {
-                Title = "Notificación",
-                Text = message,
-                Duration = 5
-            })
-        end)
-    end
-
-    if tonumber(id) and tonumber(id) ~= game.PlaceId then
-        notify("Posible script de otro juego")
-    end
-
-    local success, result = pcall(function()
-        return game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/" .. id .. ".lua")
-    end)
-
-    if success then
-        loadstring(result)()
-    else
-        notify("Error al cargar el script: " .. tostring(result))
-    end
+return function(id) loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/" .. id .. ".lua"))()
 end
