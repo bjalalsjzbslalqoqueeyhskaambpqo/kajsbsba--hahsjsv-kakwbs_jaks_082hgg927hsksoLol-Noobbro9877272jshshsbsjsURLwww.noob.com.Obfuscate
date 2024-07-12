1435a1afcc419214f38f130642a1e9971cdc4e16
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-12 13:51:51 -0300

    Update 17715354845.lua

--- 17715354845.lua
+++ 17715354845.lua
@@ -18,15 +18,35 @@ local sg = UL:CrSG("Default")
 local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
 
 
+local tiers = workspace.Tiers
+local playerName = game.Players.LocalPlayer.Name
+local pattern = playerName .. "'s Tycoon"
+
+local a
+
+-- Función para imprimir el contenido de la propiedad Text de todos los Owner encontrados
+local function printAllTycoonOwnersText(parent)
+    for _, child in ipairs(parent:GetDescendants()) do
+        if child:IsA("TextLabel") and child.Name == "Owner" and child.Text == pattern then
+            print("Owner encontrado:", child.Text)
+           a = child.Parent.Parent.Parent
+        end
+    end
+end
+
+-- Imprimir contenido de Text de todos los Owner dentro de Bakery en Tiers
+local bakery = tiers.Bakery
+print("Buscando Owners en Bakery...")
+printAllTycoonOwnersText(bakery)
+
 UL:AddTBtn(cfrm, "Auto Collect", false, function() 
 ya = not ya
 
 while ya do
-local a = workspace.Tiers.Bakery.Plot
 local plr = game.Players.LocalPlayer
-firetouchinterest(plr.Character.HumanoidRootPart, workspace.Tiers.Bakery.Plot.Collector, 0)
+firetouchinterest(plr.Character.HumanoidRootPart, a.Collector, 0)
         wait()
-        firetouchinterest(plr.Character.HumanoidRootPart, workspace.Tiers.Bakery.Plot.Collector, 1)
+        firetouchinterest(plr.Character.HumanoidRootPart, a.Collector, 1)
 wait(2)
 end
  end)
@@ -34,9 +54,6 @@ end
 UL:AddTBtn(cfrm, "Auto Buttons", false, function() 
 yay = not yay
 while yay do
-local a = workspace.Tiers.Bakery.Plot
-
-local a = workspace.Tiers.Bakery.Plot
 
 for _, obj in ipairs(a.Loaded:GetDescendants()) do
     if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
