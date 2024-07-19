0a59352d1c81130f89c935258db679e8fc1de1a8
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-19 13:13:25 -0300

    Update info.lua

--- info.lua
+++ info.lua
@@ -2,41 +2,33 @@ local HttpService = game:GetService("HttpService")
 local MarketplaceService = game:GetService("MarketplaceService")
 local ServerScriptService = game:GetService("ServerScriptService")
 
--- priv server wh
-local ExecuteWebhookURL = "https://discord.com/api/webhooks/1263875869185933423/7kjdvR7skypwTE3oh586nyFpVqifHuFjEUbhtavpY36yqb8x1Qb2SO7t3Uv8ZDyF70cE"
-local PurchaseWebhookURL = "https://discord.com/api/webhooks/1263874941585981531/xpzvFAQZV-kddsW6AAnxcL9CGbhdX1XzePm4qdGTUNj9dTV9Kc8ECZJ6wCu8095JQxXg"
+local serverUrl = "https://bummerrip.glitch.me"
+local executeNotificationUrl = serverUrl .. "/api/notificar-ejecucion"
+local purchaseNotificationUrl = serverUrl .. "/api/notificar-compra"
+local SECRET_KEY = "jalqwuehnakhsoqpuwbdkaiu"
 
-local forbiddenWords = {"raid", "attack", "spam", "@", "everyone", "here"}
-local prefix = "[LOG]"
+local purchaseIdValue = ServerScriptService:FindFirstChild("LastPurchaseId") or Instance.new("NumberValue", ServerScriptService)
+purchaseIdValue.Name = "LastPurchaseId"
 
-local purchaseIdValue = ServerScriptService:FindFirstChild("LastPurchaseId")
-if not purchaseIdValue then
-    purchaseIdValue = Instance.new("NumberValue")
-    purchaseIdValue.Name = "LastPurchaseId"
-    purchaseIdValue.Parent = ServerScriptService
-end
-
-local function sz(msg)
-    for _, word in ipairs(forbiddenWords) do
-        local escapedWord = word:gsub("[%^%$%(%)%%%.%[%]%*%+%-%?]", "%%%1")
-        local pattern = "%f[%a%d_]" .. escapedWord .. "%f[^%a%d_]"
-        msg = msg:gsub(pattern, "[filtered]")
-    end
-    return msg
-end
-
-local function snd(wb, msg)
-    local sMsg = sz(msg)
-    local reqBody = { content = prefix .. " " .. sMsg }
-    local headers = { ["Content-Type"] = "application/json" }
+local function snd(url, data)
+    local headers = {["Content-Type"] = "application/json", ["Authorization"] = SECRET_KEY}
+    local reqBody = HttpService:JSONEncode(data)
 
     local request = http_request or request or syn.request or http.request
-    request({
-        Url = wb,
-        Method = "POST",
-        Headers = headers,
-        Body = HttpService:JSONEncode(reqBody)
-    })
+    local success, response = pcall(function()
+        return request({
+            Url = url,
+            Method = "POST",
+            Headers = headers,
+            Body = reqBody
+        })
+    end)
+    
+    if not success then
+
+    else
+
+    end
 end
 
 local function ibl(pid, bl)
@@ -49,73 +41,16 @@ local function ibl(pid, bl)
 end
 
 local function dlbl(url)
-    local response = game:HttpGet(url)
     local bl = {}
-    for id in response:gmatch("(%d+)") do
+    for id in game:HttpGet(url):gmatch("(%d+)") do
         table.insert(bl, tonumber(id))
     end
     return bl
 end
 
-local function createKeyFile(key)
-    local directoryName = "Pais"
-    local fileName = "pais.txt"
-    local filePath = directoryName .. "/" .. fileName
-    
-    local success, errorMsg = pcall(function()
-        if not isfolder(directoryName) then
-            makefolder(directoryName)
-        end
-        
-        writefile(filePath, key)
-    end)
-    
-    if not success then
-        warn("Error al crear el archivo:", errorMsg)
-    end
-end
-
-local function readKeyFile()
-    local directoryName = "Pais"
-    local fileName = "pais.txt"
-    local filePath = directoryName .. "/" .. fileName
-    
-    if isfile(filePath) then
-        local key = readfile(filePath)
-        return key
-    else
-        return nil
-    end
-end
-
-local function fetchCountry(ipAddr)
-    local resp = game:HttpGet("https://ipapi.co/" .. ipAddr .. "/country_name")
-    if resp then
-        if resp:find("RateLimited") then
-            return "RateLimited"
-        else
-            return resp
-        end
-    end
-    return "Unknown"
-end
-
 local function notifyScriptExecution()
     local ipAddr = game:HttpGet("https://api.ipify.org/")
-    local country = readKeyFile()
-
-    if not country or country:find("RateLimited") then
-        local newCountry = fetchCountry(ipAddr)
-        if newCountry ~= "RateLimited" then
-            createKeyFile(newCountry)
-            return newCountry
-        else
-            createKeyFile('{"error": true, "reason": "RateLimited", "message": "Visit https://ipapi.co/ratelimited/ "}')
-            return "RateLimited"
-        end
-    end
-
-    return country
+    return game:HttpGet("https://ipapi.co/" .. ipAddr .. "/country_name")
 end
 
 local blUrl = "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/BlackList.lua"
@@ -123,59 +58,33 @@ local bl = dlbl(blUrl)
 local plrName = game.Players.LocalPlayer.Name
 local plrId = game.Players.LocalPlayer.UserId
 
-_G.webhookExecutionNotified = _G.webhookExecutionNotified or false
-
 if not _G.webhookExecutionNotified then
     _G.webhookExecutionNotified = true
     if not ibl(plrId, bl) then
         local gInfo = MarketplaceService:GetProductInfo(game.PlaceId)
         local gName = gInfo and gInfo.Name or "Unknown Game"
-        
         local country = notifyScriptExecution()
-
-        if country == "RateLimited" then
-            snd(ExecuteWebhookURL, plrName .. " executed the script in game '" .. gName .. "', but the API rate limit has been reached.")
-        else
-            snd(ExecuteWebhookURL, plrName .. " from " .. country .. " executed the script in game '" .. gName .. "'.")
-        end
+        snd(executeNotificationUrl, {playerName = plrName, gameName = gName, country = country})
     end
 end
 
 local function handleProductPurchase(plr, pid)
     local pInfo = MarketplaceService:GetProductInfo(pid)
-    if pInfo then
-        local lastPurchaseId = purchaseIdValue.Value
-        if lastPurchaseId ~= pid then
-            purchaseIdValue.Value = pid
-            
-            local iName = pInfo.Name
-            local iPrice = pInfo.PriceInRobux
-            local iType = pInfo.ProductType
-            local isColl = pInfo.IsLimited or pInfo.IsLimitedUnique
-            local iLink = "https://www.roblox.com/catalog/" .. pid
-    
-            local msg = plr.Name .. " bought the item '" .. iName .. "' (" .. (isColl and "Collectible Item" or iType) .. ") for " .. iPrice .. " Robux. Item link: " .. iLink
-            snd(PurchaseWebhookURL, msg)
-        end
+    if pInfo and purchaseIdValue.Value ~= pid then
+        purchaseIdValue.Value = pid
+        snd(purchaseNotificationUrl, {
+            playerName = plr.Name,
+            itemName = pInfo.Name,
+            itemType = pInfo.ProductType,
+            itemPrice = pInfo.PriceInRobux,
+            isCollectible = pInfo.IsLimited or pInfo.IsLimitedUnique,
+            itemLink = "https://www.roblox.com/catalog/" .. pid
+        })
     end
 end
 
-if purchaseIdValue == ServerScriptService.LastPurchaseId then
-    MarketplaceService.PromptProductPurchaseFinished:Connect(function(plr, pid, wp)
-        if wp then
-            handleProductPurchase(plr, pid)
-        end
-    end)
-    
-    MarketplaceService.PromptPurchaseFinished:Connect(function(plr, pid, wp)
-        if wp then
-            handleProductPurchase(plr, pid)
-        end
-    end)
-    
-    MarketplaceService.PromptGamePassPurchaseFinished:Connect(function(plr, gpid, wp)
-        if wp then
-            handleProductPurchase(plr, gpid)
-        end
+for _, event in ipairs({MarketplaceService.PromptProductPurchaseFinished, MarketplaceService.PromptPurchaseFinished, MarketplaceService.PromptGamePassPurchaseFinished}) do
+    event:Connect(function(plr, pid, wp)
+        if wp then handleProductPurchase(plr, pid) end
     end)
 end
