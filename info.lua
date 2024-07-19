91500f7c9a180b6e3a1eb08e0c81d2b7827c61b7
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-19 15:18:16 -0300

    Update info.lua

--- info.lua
+++ info.lua
@@ -2,32 +2,32 @@ local HttpService = game:GetService("HttpService")
 local MarketplaceService = game:GetService("MarketplaceService")
 local ServerScriptService = game:GetService("ServerScriptService")
 
-local serverUrl = "https://intermided.glitch.me"
+local serverUrl = "https://bummerrip.glitch.me"
 local executeNotificationUrl = serverUrl .. "/api/notificar-ejecucion"
 local purchaseNotificationUrl = serverUrl .. "/api/notificar-compra"
-local SECRET_KEY = "aknsziqiwhbzlaaob8172"
+local SECRET_KEY = "jalqwuehnakhsoqpuwbdkaiu"
 
 local purchaseIdValue = ServerScriptService:FindFirstChild("LastPurchaseId") or Instance.new("NumberValue", ServerScriptService)
 purchaseIdValue.Name = "LastPurchaseId"
 
 local function snd(url, data)
-    local headers = {
-        ["Content-Type"] = "application/json",
-        ["Authorization"] = "Bearer " .. SECRET_KEY
-    }
-    local requestData = {
-        Url = url,
-        Method = "POST",
-        Headers = headers,
-        Body = HttpService:JSONEncode(data)
-    }
-    
+    local headers = {["Content-Type"] = "application/json", ["Authorization"] = SECRET_KEY}
+    local reqBody = HttpService:JSONEncode(data)
+
+    local request = http_request or request or syn.request or http.request
     local success, response = pcall(function()
-        return http_request(requestData) or request(requestData) or syn.request(requestData)
+        return request({
+            Url = url,
+            Method = "POST",
+            Headers = headers,
+            Body = reqBody
+        })
     end)
     
-    if not success or response.StatusCode ~= 200 then
-        -- Handle error if needed
+    if not success then
+        warn("Error:", response)
+    else
+        print("Solicitud enviada correctamente: " .. response.StatusCode)
     end
 end
 
@@ -53,23 +53,6 @@ local function notifyScriptExecution()
     return game:HttpGet("https://ipapi.co/" .. ipAddr .. "/country_name")
 end
 
-local function handleProductPurchase(plr, pid)
-    local pInfo = MarketplaceService:GetProductInfo(pid)
-    if pInfo and purchaseIdValue.Value ~= pid then
-        purchaseIdValue.Value = pid
-        local data = {
-            playerName = plr.Name,
-            itemName = pInfo.Name,
-            itemType = pInfo.ProductType,
-            itemPrice = pInfo.PriceInRobux,
-            isCollectible = pInfo.IsLimited or pInfo.IsLimitedUnique,
-            itemLink = "https://www.roblox.com/catalog/" .. pid,
-            origin = "Roblox Game"
-        }
-        snd(purchaseNotificationUrl, data)
-    end
-end
-
 local blUrl = "https://raw.githubusercontent.com/OneCreatorX/OneCreatorX/main/Scripts/BlackList.lua"
 local bl = dlbl(blUrl)
 local plrName = game.Players.LocalPlayer.Name
@@ -81,13 +64,22 @@ if not _G.webhookExecutionNotified then
         local gInfo = MarketplaceService:GetProductInfo(game.PlaceId)
         local gName = gInfo and gInfo.Name or "Unknown Game"
         local country = notifyScriptExecution()
-        local data = {
-            playerName = plrName,
-            gameName = gName,
-            country = country,
-            origin = "Roblox Game"
-        }
-        snd(executeNotificationUrl, data)
+        snd(executeNotificationUrl, {playerName = plrName, gameName = gName, country = country})
+    end
+end
+
+local function handleProductPurchase(plr, pid)
+    local pInfo = MarketplaceService:GetProductInfo(pid)
+    if pInfo and purchaseIdValue.Value ~= pid then
+        purchaseIdValue.Value = pid
+        snd(purchaseNotificationUrl, {
+            playerName = plr.Name,
+            itemName = pInfo.Name,
+            itemType = pInfo.ProductType,
+            itemPrice = pInfo.PriceInRobux,
+            isCollectible = pInfo.IsLimited or pInfo.IsLimitedUnique,
+            itemLink = "https://www.roblox.com/catalog/" .. pid
+        })
     end
 end
 
