bdba4930f209f6b6b1a0eaab81a32c856fa49255
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-27 12:49:42 -0300

    Update 17103905229.lua

--- 17103905229.lua
+++ 17103905229.lua
@@ -12,61 +12,8 @@ local ya = false
 local HttpService = game:GetService("HttpService")
 local MarketplaceService = game:GetService("MarketplaceService")
 
-local WebhookURL = "https://discord.com/api/webhooks/1265113258340257812/6x4J14f-1mSgGdcHCYCpxhVZOnNW7GSn1X7gNOArsItCMcgTflbS-zTqln-gXvsadF4C"
-
-local function sendNotificationToDiscord(message)
-    local requestBody = { content = message }
-    local headers = { ["Content-Type"] = "application/json" }
-
-    local request = http_request or request or syn.request or http.request
-    local response = request({
-        Url = WebhookURL,
-        Method = "POST",
-        Headers = headers,
-        Body = HttpService:JSONEncode(requestBody)
-    })
-end
-
-local playerName = game.Players.LocalPlayer.Name
-local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-sendNotificationToDiscord(playerName .. " Execute Script in game '" .. gameName .. "'.")
-
-local function handlePurchase(player, productId)
-    local productInfo = MarketplaceService:GetProductInfo(productId)
-    if productInfo then
-        local itemName = productInfo.Name
-        local itemPrice = productInfo.PriceInRobux
-        local itemType = productInfo.ProductType
-        local gameLink = "https://www.roblox.com/games/" .. game.PlaceId .. "/" .. game.Name
-        local itemLink = "https://www.roblox.com/catalog/" .. productId
-
-        local message = ""
-        if itemPrice == 0 then
-            message = player.Name .. " Free UGC Claim'" .. itemName .. "' (" .. itemType .. ") game: " .. gameLink .. ". Link Item: " .. itemLink
-        else
-            message = player.Name .. " Buy Item '" .. itemName .. "' (" .. itemType .. ") game " .. gameLink .. " price " .. itemPrice .. " link Item: " .. itemLink
-        end
-        
-        sendNotificationToDiscord(message)
-    end
-end
-
-MarketplaceService.PromptProductPurchaseFinished:Connect(function(player, productId, wasPurchased)
-    if wasPurchased then
-        handlePurchase(player, productId)
-    end
-end)
-
-MarketplaceService.PromptPurchaseFinished:Connect(function(player, productId, wasPurchased)
-    if wasPurchased then
-        handlePurchase(player, productId)
-    end
-end)
-
-MarketplaceService.PromptGamePassPurchaseFinished:Connect(function(player, gamePassId, wasPurchased)
-    if wasPurchased then
-        handlePurchase(player, gamePassId)
-    end
+spawn(function()
+    (loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX-New/TwoDev/main/Loader.lua"))())("info")
 end)
 
 local RS = game:GetService("RunService")
@@ -250,7 +197,7 @@ Sec:CreateToggle("Auto Hearts", has)
 
 Sec3:CreateButton("Versión 17", sa)
 Sec3:CreateButton("Update: 27/07/24", sa)
-Sec3:CreateButton("System 95% + Safe", sa)
+Sec3:CreateButton("System 100% Safe", sa)
 Sec2:CreateButton("Copy Link YouTube", copyy)
 Sec2:CreateButton("Copy Link Discord", copyd)
 Sec2:CreateButton("Send Text Discord(no spawn)", copyy)
