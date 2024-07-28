f98bbe904ca69fbf958b303578ee294838f01294
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-28 00:17:23 -0300

    Update 17103905229.lua

--- 17103905229.lua
+++ 17103905229.lua
@@ -6,7 +6,7 @@ local Sec2 = Win:NewSection("Credits: OneCreatorX")
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local b = true
-local speed = 55
+local speed = 50
 local ya = true
 
 spawn(function()
