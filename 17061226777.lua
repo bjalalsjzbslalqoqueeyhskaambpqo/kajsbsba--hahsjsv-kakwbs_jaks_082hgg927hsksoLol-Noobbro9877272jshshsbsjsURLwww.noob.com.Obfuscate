c8fa8e12a08e8c23cfe34bfabe4f3fe6374159ed
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-07-28 16:05:27 -0300

    Create 17061226777.lua

---
+++ 17061226777.lua
@@ -0,0 +1,73 @@
+local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/MyLibrery.lua"))()
+
+local gameName = ""
+if gameName == "" then
+    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+end
+
+local function cleanGameName(name)
+    name = name:gsub("%b[]", "")
+    name = name:match("^[^:]*")
+    return name:match("^%s*(.-)%s*$")
+end
+
+gameName = cleanGameName(gameName)
+
+local p = game.Players.LocalPlayer
+local sg = UL:CrSG("Default")
+local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
+
+
+UL:AddTBtn(cfrm, "Auto Obbis", false, function()
+a = not a
+while a do
+
+for _, obj in workspace.Checkpoints:GetChildren() do
+    if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
+
+local plr = game.Players.LocalPlayer
+firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
+        wait()
+        firetouchinterest(plr.Character.HumanoidRootPart, obj, 1)
+end end
+wait(5)
+end
+end)
+
+UL:AddTBtn(cfrm, "Auto Blocks", false, function()
+b = not b
+while b do
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+local spawners = workspace.Spawners:GetChildren()
+
+for _, spawner in pairs(spawners) do
+    if spawner:IsA("BasePart") then
+        player.Character:MoveTo(spawner.Position)
+wait(0.2)
+        for _, descendant in pairs(spawner:GetDescendants()) do
+            if descendant:IsA("ProximityPrompt") then
+                fireproximityprompt(descendant)
+wait(0.2)
+            end
+        end
+    end
+end
+wiat(5)
+end
+end)
+
+
+UL:AddText(crFrm, "By Script: OneCreatorX ")
+UL:AddText(crFrm, "Create Script: 28/07/24 ")
+UL:AddText(crFrm, "Update Script: --/--/--")
+UL:AddText(crFrm, "Script Version: 0.1")
+UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
+UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
+
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+end)
