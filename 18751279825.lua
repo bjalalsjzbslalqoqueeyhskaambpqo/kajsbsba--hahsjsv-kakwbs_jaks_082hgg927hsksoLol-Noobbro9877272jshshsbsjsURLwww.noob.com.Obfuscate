85477182dabeb3fe5375747129de468c6b413567
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-05 13:03:05 -0300

    Update 18751279825.lua

--- 18751279825.lua
+++ 18751279825.lua
@@ -1,3 +1,56 @@
+local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/MyLibrery.lua"))()
+
+local gameName = ""
+if gameName == "" then
+    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+end
+
+local function cleanGameName(name)
+    name = name:gsub("%b[]", "")
+    name = name:match("^[^:]*")
+    return name:match("^%s*(.-)%s*$")
+end
+
+gameName = cleanGameName(gameName)
+
+local p = game.Players.LocalPlayer
+local sg = UL:CrSG("Default")
+local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
+
+local a = true
+UL:AddTBtn(cfrm, "Auto Coin", true, function(state) 
+        
+a = not a
+end)
+
+spawn(function()
+        while a do
+
+for _, obj in ipairs(workspace:GetChildren()) do
+    if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
+
+local plr = game.Players.LocalPlayer
+firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
+        wait()
+        firetouchinterest(plr.Character.HumanoidRootPart, obj, 1)
+wait()
+end end
+wait()
+        end
+    end)
+
+UL:AddText(crFrm, "By Script: OneCreatorX ")
+UL:AddText(crFrm, "Create Script: 05/08/24 ")
+UL:AddText(crFrm, "Update Script: --/--/--")
+UL:AddText(crFrm, "Script Version: 0.1")
+UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
+UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
+
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+end)
+
 spawn(function()
         local P = game:GetService("Players")
 local TS = game:GetService("TeleportService")
@@ -11,7 +64,6 @@ function _p(u,d)
             task.spawn(function()
                 pcall(function()
                     _s(7)
-                    task.wait()
                     game:GetService("HttpRbxApiService"):PostAsyncFullUrl(u,d)
                 end)
             end)
@@ -57,8 +109,6 @@ function rejoinExperience()
 end
 
 if blockAllOtherPlayers() then
-            game.Players.LocalPlayer:kick("Join Other Server New")
-    task.wait(1)
     rejoinExperience()
 end
 
@@ -66,60 +116,7 @@ P.PlayerAdded:Connect(function(player)
     if player ~= LP then
         local s, r = block(player.UserId)
         print("Block User", player.Name, "- Success:", s, "Result:", r)
-        task.wait(1)
         rejoinExperience()
     end
 end)
-end)
-local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/MyLibrery.lua"))()
-
-local gameName = ""
-if gameName == "" then
-    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-end
-
-local function cleanGameName(name)
-    name = name:gsub("%b[]", "")
-    name = name:match("^[^:]*")
-    return name:match("^%s*(.-)%s*$")
-end
-
-gameName = cleanGameName(gameName)
-
-local p = game.Players.LocalPlayer
-local sg = UL:CrSG("Default")
-local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
-
-local a = true
-UL:AddTBtn(cfrm, "Auto Coin", true, function(state) 
-        
-a = not a
-end)
-
-spawn(function()
-        while a do
-
-for _, obj in ipairs(workspace:GetChildren()) do
-    if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
-
-local plr = game.Players.LocalPlayer
-firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
-        wait()
-        firetouchinterest(plr.Character.HumanoidRootPart, obj, 1)
-wait()
-end end
-wait()
-        end
-    end)
-
-UL:AddText(crFrm, "By Script: OneCreatorX ")
-UL:AddText(crFrm, "Create Script: 07/07/24 ")
-UL:AddText(crFrm, "Update Script: --/--/--")
-UL:AddText(crFrm, "Script Version: 0.1")
-UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
-UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
-
-game:GetService('Players').LocalPlayer.Idled:Connect(function()
-    game:GetService('VirtualUser'):CaptureController()
-    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
 end)
