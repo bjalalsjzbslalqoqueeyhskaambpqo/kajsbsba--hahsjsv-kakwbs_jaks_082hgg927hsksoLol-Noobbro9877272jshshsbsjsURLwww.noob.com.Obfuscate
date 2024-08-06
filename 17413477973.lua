152011e7c6baa3a9dca982dca8b8c167173ad184
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-05 21:19:49 -0300

    Create 17413477973.lua

---
+++ 17413477973.lua
@@ -0,0 +1,61 @@
+local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/MyLibrery.lua"))()
+
+local gameName = ""
+if gameName == "" then
+    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+end
+
+local function cleanGameName(name)
+    name = name:gsub("%b[]", "")
+    name = name:match("^[^:]*")
+    return name:match("^%s*(.-)%s*$")
+end
+
+gameName = cleanGameName(gameName)
+
+local p = game.Players.LocalPlayer
+local sg = UL:CrSG("Default")
+local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
+
+
+
+UL:AddBtn(cfrm, "Auto Buyer", function() 
+(loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX-New/TwoDev/main/Loader.lua"))())("Auto%20Buyer(Fast%20Claim)")
+
+end)
+
+UL:AddTBtn(cfrm, "Instant Codes", false, function()
+a = not a
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+while a do
+    local response = game:HttpGet("https://codes.matiastoledo284.workers.dev")
+    local data = HttpService:JSONDecode(response)
+    local codes = data.table
+    
+    for _, code in ipairs(codes) do
+        
+        local args = { tostring(code) }
+        
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("UGCService"):WaitForChild("RF"):WaitForChild("ClaimItem"):InvokeServer(unpack(args))
+
+        wait()
+    end
+    
+    wait(0.1)
+end
+
+end)
+
+UL:AddText(crFrm, "By Script: OneCreatorX ")
+UL:AddText(crFrm, "Create Script: 05/08/24 ")
+UL:AddText(crFrm, "Update Script: --/--/--")
+UL:AddText(crFrm, "Script Version: 0.2")
+UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
+UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
+
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
+    game:GetService('VirtualUser'):CaptureController()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+end)
