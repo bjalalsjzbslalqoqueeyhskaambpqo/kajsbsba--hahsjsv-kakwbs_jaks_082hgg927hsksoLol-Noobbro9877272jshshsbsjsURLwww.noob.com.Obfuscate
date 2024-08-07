3286dcaeef5ddcf51701d6de951ec715dc94e5d3
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-07 16:09:27 -0300

    Update 17413477973.lua

--- 17413477973.lua
+++ 17413477973.lua
@@ -1,3 +1,4 @@
+
 local HttpService = game:GetService("HttpService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/MyLibrery.lua"))()
@@ -9,7 +10,7 @@ local p = game.Players.LocalPlayer
 local sg = UL:CrSG("Default")
 local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
 
-local statusText = UL:AddText(crFrm, "Status: Unknown")
+local statusText = UL:AddText(crFrm, "Status: Idle")
 
 UL:AddBtn(cfrm, "Auto Buyer", function() 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX-New/TwoDev/main/Loader.lua"))()("Auto%20Buyer(Fast%20Claim)")
@@ -20,9 +21,7 @@ local codes = {}
 UL:AddTBox(cfrm, "Enter Codes:", function(text)
     codes = {}
     for code in text:gmatch("[^\r\n]+") do
-        for c in code:gmatch("%S+") do
-            table.insert(codes, c)
-        end
+        table.insert(codes, code:match("^%s*(.-)%s*$"))
     end
 end)
 
@@ -34,21 +33,25 @@ UL:AddTBtn(cfrm, "Process Codes", false, function()
     
     spawn(function()
         while isProcessingCodes do
+            statusText.Text = "Status: Processing Codes"
             for _, code in ipairs(codes) do
                 pcall(function()
-                    ugcService:InvokeServer(tostring(code))
+                    ugcService:InvokeServer(string.format('"%s"', code))
+                    print("Sent code:", code)
                 end)
                 wait(0.1)
             end
-            wait(0.2)
+            statusText.Text = "Status: Waiting"
+            wait(0.4)
         end
+        statusText.Text = "Status: Idle"
     end)
 end)
 
 UL:AddText(crFrm, "By Script: OneCreatorX ")
 UL:AddText(crFrm, "Create Script: 05/08/24 ")
-UL:AddText(crFrm, "Update Script: --/--/--")
-UL:AddText(crFrm, "Script Version: 0.2")
+UL:AddText(crFrm, "Update Script: 07/08/24")
+UL:AddText(crFrm, "Script Version: 0.3")
 UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
 UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
 
