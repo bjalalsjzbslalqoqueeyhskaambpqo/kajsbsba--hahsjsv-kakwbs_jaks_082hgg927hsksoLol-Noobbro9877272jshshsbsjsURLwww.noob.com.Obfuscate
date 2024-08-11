76a2435dfad3e08b2786b3ab12f623288d4d427f
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-11 01:09:42 -0300

    Update 18674510760.lua

--- 18674510760.lua
+++ 18674510760.lua
@@ -18,21 +18,56 @@ local sg = UL:CrSG("Default")
 local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
 
 
-UL:AddTBtn(cfrm, "Spin 100k", false, function()
-a = not a
-while a do
+UL:AddBtn(cfrm, "Claim 100k", function()
 
 local args = {
     [1] = "Reward7"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Spin"):FireServer(unpack(args))
-wait(1)
-end
 
 end)
 
 
+UL:AddBtn(cfrm, "U Can Claim Info", function()
+        
+local info = game.Players.LocalPlayer.PlayerStats.Spin.Value
+local time = game.Players.LocalPlayer.PlayerStats.SpinTime.Value
+
+local function formatTime(seconds)
+    local minutes = math.floor(seconds / 60)
+    local remainingSeconds = seconds % 60
+    if minutes > 0 then
+        return string.format("%02d:%02d", minutes, remainingSeconds)
+    else
+        return string.format("%02d", remainingSeconds)
+    end
+end
+
+local formattedTime = formatTime(time)
+
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "Next in: " .. formattedTime,
+    Text = "+100K",
+    Duration = 5,
+})
+
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "U Can Claim",
+    Text = info .. "00K",
+    Duration = 5,
+})
+
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "recommendation to claim",
+    Text = "Is 500k 👀",
+    Duration = 5,
+})
+
+    end)
 
 
     
@@ -41,8 +76,8 @@ end)
 
 UL:AddText(crFrm, "By Script: OneCreatorX ")
 UL:AddText(crFrm, "Create Script: 09/08/24 ")
-UL:AddText(crFrm, "Update Script: --/--/--")
-UL:AddText(crFrm, "Script Version: 0.1")
+UL:AddText(crFrm, "Update Script: 11/08/24")
+UL:AddText(crFrm, "Script Version: 0.2")
 UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
 UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
 
