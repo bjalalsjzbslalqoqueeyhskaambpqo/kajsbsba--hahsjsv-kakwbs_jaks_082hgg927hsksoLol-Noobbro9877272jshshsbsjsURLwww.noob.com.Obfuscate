76c689f884b4ce24d9396091da9bd26b8e5d57d7
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-12 12:07:48 -0300

    Update 17103905229.lua

--- 17103905229.lua
+++ 17103905229.lua
@@ -283,19 +283,6 @@ function tc()
     end
 end
 
-Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-W = Lib:NewWindow("Collect For UGC")
-S = W:NewSection("Options")
-
-S:CreateToggle("Auto Collect", tc)
-S:CreateTextbox("Speed Use: 40 or 43 ", function(v)
-    s = tonumber(v) or s
-    if h then h.WalkSpeed = s end
-end)
-S:CreateTextbox("Height Tolerance: 3-10", function(v)
-    ht = tonumber(v) or ht
-    aht()
-end)
 
 L.CharacterAdded:Connect(function(char)
     wait(1)
@@ -315,6 +302,48 @@ L.Idled:Connect(function()
     game:GetService('VirtualUser'):CaptureController()
     game:GetService('VirtualUser'):ClickButton2(Vector2.new())
 end)
+
+local UL = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/MyLibrery.lua"))()
+
+local gameName = ""
+if gameName == "" then
+    gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+end
+
+local function cleanGameName(name)
+    name = name:gsub("%b[]", "")
+    name = name:match("^[^:]*")
+    return name:match("^%s*(.-)%s*$")
+end
+
+gameName = cleanGameName(gameName)
+
+local p = game.Players.LocalPlayer
+local sg = UL:CrSG("Default")
+local frm, cfrm, crFrm = UL:CrFrm(sg, gameName)
+
+
+UL:AddTBtn(cfrm, "Auto Walk Collect", false, tc)
+
+UL:AddTBox(cfrm, "Speed use: 40-45", function(v) 
+ s = tonumber(v) or s
+    if h then h.WalkSpeed = s end
+end)
+
+
+UL:AddTBox(cfrm, "Height Tolerance: 3-10", function(ju) 
+ ht = tonumber(ju) or ht
+    aht()
+end)
+
+UL:AddText(crFrm, "By Script: OneCreatorX ")
+UL:AddText(crFrm, "Create Script: 12/08/24 ")
+UL:AddText(crFrm, "Update Script: --/--/--")
+UL:AddText(crFrm, "Script Version: 0.1")
+UL:AddText(crFrm, "Safe %: 100")
+UL:AddBtn(crFrm, "Copy link YouTube", function() setclipboard("https://youtube.com/@onecreatorx") end)
+UL:AddBtn(crFrm, "Copy link Discord", function() setclipboard("https://discord.com/invite/UNJpdJx7c4") end)
+
 
 uc()
 sph()
