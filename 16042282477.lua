c572b434c67fdb098bba1a4f812b7e43e737fa04
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-30 21:38:18 -0300

    Create 16042282477.lua

---
+++ 16042282477.lua
@@ -0,0 +1,36 @@
+local MiniUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/go.lua"))()
+
+local ui = MiniUI:new()
+
+ui:TBtn("Inf Wins", function()
+  a = not a
+while a do
+
+
+
+for _, obj in ipairs(workspace.Race:GetDescendants()) do
+    if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
+
+local plr = game.Players.LocalPlayer
+firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
+        wait()
+        firetouchinterest(plr.Character.HumanoidRootPart, obj, 1)
+end end
+wait()
+end  
+end)
+
+
+
+wait(0.7)
+infoSub = ui:Sub("Info Script")
+infoSub:Txt("Version: 0.1")
+infoSub:Txt("Create: 20/08/24")
+infoSub:Txt("Update: -/-/-")
+infoSub:Btn("Link YouTube", function()
+   setclipboard("https://youtube.com/@onecreatorx") 
+end)
+
+infoSub:Btn("Link Discord", function()
+  setclipboard("https://discord.com/invite/UNJpdJx7c4")  
+end)
