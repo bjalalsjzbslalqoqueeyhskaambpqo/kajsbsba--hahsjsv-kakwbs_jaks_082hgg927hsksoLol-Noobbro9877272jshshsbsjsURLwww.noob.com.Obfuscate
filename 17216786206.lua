60e17b1173e2b4e3ea90777c759d46ddbb6615c1
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-08-30 22:07:58 -0300

    Create 17216786206.lua

---
+++ 17216786206.lua
@@ -0,0 +1,37 @@
+local MiniUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/go.lua"))()
+
+local ui = MiniUI:new()
+
+ui:TBtn("Fast Collect Stars", function()
+  a = not a
+while a do
+
+spawn(function()
+
+for _, obj in ipairs(workspace.Collectables:GetDescendants()) do
+    if obj:IsA("BasePart") and obj:FindFirstChild("TouchInterest") then
+
+local plr = game.Players.LocalPlayer
+firetouchinterest(plr.Character.HumanoidRootPart, obj, 0)
+        wait()
+        firetouchinterest(plr.Character.HumanoidRootPart, obj, 1)
+end end
+end)
+wait()
+end  
+end)
+
+
+
+wait(0.7)
+infoSub = ui:Sub("Info Script")
+infoSub:Txt("Version: 0.1")
+infoSub:Txt("Create: 30/08/24")
+infoSub:Txt("Update: -/-/-")
+infoSub:Btn("Link YouTube", function()
+   setclipboard("https://youtube.com/@onecreatorx") 
+end)
+
+infoSub:Btn("Link Discord", function()
+  setclipboard("https://discord.com/invite/UNJpdJx7c4")  
+end)
