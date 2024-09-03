842dd6542bb4cd78e1249023290afe902785a63d
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-09-03 18:16:18 -0300

    Create 17269184313.lua

---
+++ 17269184313.lua
@@ -0,0 +1,30 @@
+local MiniUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/go.lua"))()
+
+local ui = MiniUI:new()
+
+
+ui:TBox("Coins/monedas", function(text)
+    local args = {
+    [1] = text
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.7.0"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("CoinService"):WaitForChild("RF"):WaitForChild("ChangeCoin"):InvokeServer(unpack(args))
+
+end)
+
+
+
+
+
+wait(0.7)
+infoSub = ui:Sub("Info Script")
+infoSub:Txt("Version: 0.1")
+infoSub:Txt("Create: 20/08/24")
+infoSub:Txt("Update: -/-/-")
+infoSub:Btn("Link YouTube", function()
+   setclipboard("https://youtube.com/@onecreatorx") 
+end)
+
+infoSub:Btn("Link Discord", function()
+  setclipboard("https://discord.com/invite/UNJpdJx7c4")  
+end)
