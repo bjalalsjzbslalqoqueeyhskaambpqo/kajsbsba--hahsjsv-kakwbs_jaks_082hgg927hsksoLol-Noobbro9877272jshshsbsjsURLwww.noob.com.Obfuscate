34bae1ea72417b53d4f0941f14499e66003d2004
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-09-15 17:20:19 -0300

    Create 18729168233.lua

---
+++ 18729168233.lua
@@ -0,0 +1,52 @@
+local MiniUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/go.lua"))()
+
+local ui = MiniUI:new()
+local a = false
+ui:TBtn("Inf", function()
+    a = not a
+while a do
+
+
+local args = {
+    [1] = "4kD"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Rewards"):WaitForChild("Claim"):FireServer(unpack(args))
+
+
+local args = {
+    [1] = "150kM"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Rewards"):WaitForChild("Claim"):FireServer(unpack(args))
+
+
+wait()
+end
+end)
+
+
+local a = false
+ui:TBtn("Rebirth", function()
+    a = not a
+while a do
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Events"):WaitForChild("Rebirth"):FireServer()
+wait(1)
+end
+end)
+
+
+
+wait(0.7)
+infoSub = ui:Sub("Info Script")
+infoSub:Txt("Version: 0.1")
+infoSub:Txt("Create: 15/09/24")
+infoSub:Txt("Update: -/-/-")
+infoSub:Btn("Link YouTube", function()
+   setclipboard("https://youtube.com/@onecreatorx") 
+end)
+
+infoSub:Btn("Link Discord", function()
+  setclipboard("https://discord.com/invite/UNJpdJx7c4")  
+end)
+ 
