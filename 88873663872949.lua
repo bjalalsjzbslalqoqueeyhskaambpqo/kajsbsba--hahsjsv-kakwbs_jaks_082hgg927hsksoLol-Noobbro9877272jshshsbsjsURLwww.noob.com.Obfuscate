5fd1667ab3467355c4688e3bbdffae7d00b5c475
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-09-18 09:54:40 -0300

    Create 88873663872949.lua

---
+++ 88873663872949.lua
@@ -0,0 +1,52 @@
+local MiniUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/bjalalsjzbslalqoqueeyhskaambpqo/kajsbsba--hahsjsv-kakwbs_jaks_082hgg927hsksoLol-Noobbro9877272jshshsbsjsURLwww.noob.com.Obfuscate/main/go.lua"))()
+
+local ui = MiniUI:new()
+
+ui:TBtn("Inf", function()
+a = not a
+ui:Notify(" (warn do not exceed the limit shown on the leaderboards)", 10)
+while a do
+local args = {
+    [1] = "Strength",
+    [2] = 100000
+}
+
+game:GetService("StarterPlayer"):WaitForChild("StarterCharacterScripts"):WaitForChild("Remotes"):WaitForChild("leaderstatsEvent"):FireServer(unpack(args))
+
+
+local args = {
+    [1] = "Rebirth",
+    [2] = 25
+}
+
+game:GetService("StarterPlayer"):WaitForChild("StarterCharacterScripts"):WaitForChild("Remotes"):WaitForChild("leaderstatsEvent"):FireServer(unpack(args))
+
+
+local args = {
+    [1] = "Wins",
+    [2] = 10000
+}
+
+game:GetService("StarterPlayer"):WaitForChild("StarterCharacterScripts"):WaitForChild("Remotes"):WaitForChild("leaderstatsEvent"):FireServer(unpack(args))
+
+
+wait()
+end
+end)
+
+
+
+
+wait(0.7)
+infoSub = ui:Sub("Info Script")
+infoSub:Txt("Version: 0.1")
+infoSub:Txt("Create: 20/09/24")
+infoSub:Txt("Update: -/-/-")
+infoSub:Btn("Link YouTube", function()
+   setclipboard("https://youtube.com/@onecreatorx") 
+end)
+
+infoSub:Btn("Link Discord", function()
+  setclipboard("https://discord.com/invite/UNJpdJx7c4")  
+end)
+ 
