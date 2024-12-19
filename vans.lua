84e6a2347497bf1e0ed1b829cf3ea516a8808287
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2024-12-19 11:50:16 -0300

    Update vans.lua

--- vans.lua
+++ vans.lua
@@ -3,17 +3,16 @@ local MiniUI = loadstring(game:HttpGet("https://ui.api-x.site"))()
 local ui = MiniUI:new()
 
 ui:Btn("Instant UGC", function()
-    
+    ui:Notify("So Wait")
 for _, obj in ipairs(workspace:GetDescendants()) do
     if obj:IsA("BasePart") and obj.Name == "Coconut_Outside" then
 
 local plr = game.Players.LocalPlayer
  obj.Position = plr.Character.HumanoidRootPart.Position
         wait()
-
 end end
 
-wait(1)
+wait(2)
 local args = {
     [1] = "CoconaCoconutHunt",
     [2] = 1
@@ -25,6 +24,13 @@ wait(1)
 local args = {     [1] = "CoconaCoconutHunt",     [2] = 1 }  game:GetService("ReplicatedStorage"):WaitForChild("DeliverHunt_collectedInstanceFunction"):InvokeServer(unpack(args))
 end)
 
+ui:Btn("Tp Tokyo", function()
+local plr = game.Players.LocalPlayer
+ plr.Character:MoveTo(Vector3.new(26, 4, 2753)
+        
+
+
+    end)
 
 wait(0.7)
 infoSub = ui:Sub("Info Script")
