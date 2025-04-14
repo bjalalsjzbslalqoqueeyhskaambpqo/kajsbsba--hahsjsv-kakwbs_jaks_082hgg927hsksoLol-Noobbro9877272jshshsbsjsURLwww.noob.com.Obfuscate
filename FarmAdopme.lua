dba3ad721187b4660f1ba1d19a1d9c6ffa06ec38
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2025-04-14 18:08:06 -0300

    Update FarmAdopme.lua

--- FarmAdopme.lua
+++ FarmAdopme.lua
@@ -292,6 +292,27 @@ workspace:WaitForChild("Interiors").ChildAdded:Connect(function(child)
 end)
 
 
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+local NetworkClient = game:GetService("NetworkClient")
+local VirtualUser = game:GetService("VirtualUser")
+
+NetworkClient.ChildRemoved:Connect(function()
+    if #Players:GetPlayers() <= 1 then
+        Players.LocalPlayer:Kick("\nReconectando automáticamente...")
+        task.wait(1)
+        TeleportService:Teleport(game.PlaceId, Players.LocalPlayer)
+    else
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
+    end
+end)
+
+Players.LocalPlayer.Idled:Connect(function()
+    VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+end)
+
 local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
 local p = Players.LocalPlayer
