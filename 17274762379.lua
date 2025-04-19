ab0684356679c4646b8dc7059ee7b7af5393d9f5
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2025-04-19 19:29:20 -0300

    Update 17274762379.lua

--- 17274762379.lua
+++ 17274762379.lua
@@ -4,7 +4,13 @@ local mouse = plr:GetMouse()
 
 workspace.ChildAdded:Connect(function(c)
     if c.Name == "fishing" then
-        while c and c.Parent do
+        local alive = true
+        c.AncestryChanged:Connect(function(_, parent)
+            if not parent then
+                alive = false
+            end
+        end)
+        while alive do
             vim:SendMouseButtonEvent(mouse.X, mouse.Y, 0, true, nil, 0)
             task.wait(0.05)
             vim:SendMouseButtonEvent(mouse.X, mouse.Y, 0, false, nil, 0)
